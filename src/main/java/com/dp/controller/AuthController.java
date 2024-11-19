e40ad16f1ce58b8649bc749d11c3e7fd8fe5297c
DpGautham772 <124214120+DpGautham772@users.noreply.github.com>
2024-11-19 10:57:05 -0600

    signIN method is created in AuthController

--- src/main/java/com/dp/controller/AuthController.java
+++ src/main/java/com/dp/controller/AuthController.java
@@ -2,21 +2,30 @@ package com.dp.controller;
 
 import com.dp.config.JwtProvider;
 import com.dp.model.Cart;
+import com.dp.model.USER_ROLE;
 import com.dp.model.User;
 import com.dp.repository.CartRepository;
 import com.dp.repository.UserRepository;
+import com.dp.request.LoginRequest;
 import com.dp.response.AuthResponse;
 import com.dp.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/auth")
@@ -33,6 +42,7 @@ public class AuthController {
     @Autowired
     private CartRepository cartRepository;
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
         User isEmailExist = userRepository.findByEmail(user.getEmail());
         if (isEmailExist!=null){
@@ -61,6 +71,42 @@ public class AuthController {
         authResponse.setRole(savedUser.getRole());
 
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin(@RequestBody LoginRequest req){
+
+        String username= req.getEmail();
+        String password = req.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.isEmpty()?null:authorities.iterator().next().getAuthority();
+
+        String jwt = jwtProvider.GenerateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register Success");
+
+       authResponse.setRole(USER_ROLE.valueOf(role));
+
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) {
+
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid User");
+        }
+        if(!passwordEncoder.matches(password, userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid Password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 
 }
