d80af5f661aa39fc4a45c663f69071c8da872736
DpGautham772 <124214120+DpGautham772@users.noreply.github.com>
2024-11-19 10:57:05 -0600

    signup method is created in AuthController

--- src/main/java/com/dp/controller/AuthController.java
+++ src/main/java/com/dp/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,7 @@ public class AuthController {
     @Autowired
     private CartRepository cartRepository;
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
         User isEmailExist = userRepository.findByEmail(user.getEmail());
         if (isEmailExist!=null){
